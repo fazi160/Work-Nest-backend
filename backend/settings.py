9c4d32cabed98cce81361cc8d711e2e961846cac
fazil <fazifazil160160@gmail.com>
2024-12-03 00:20:49 +0530

    settings updated

--- backend/settings.py
+++ backend/settings.py
@@ -224,7 +224,8 @@ CORS_ALLOW_CREDENTIALS = True
 
 CORS_ALLOWED_ORIGINS = [
     'http://localhost:5173',
-    'http://127.0.0.1:5173',    
+    'http://127.0.0.1:5173',  
+    'https://worknest.conqod.site/'  
 ]
 CHANNEL_LAYERS = {
     'default': {
